72094f98bce4491d4c674628ff174866cd61534f
zsybupt <zsybupt@outlook.com>
2019-07-10 23:46:36 +0800

    add interface

---
+++ src/Container/ConcurrentContainer/IConcurrentStack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcurrentContainer
+{
+    public interface IConcurrentStack<T>
+    {
+        void Push(T value);
+
+        T Pop();
+
+        T Peek();
+    }
+}
